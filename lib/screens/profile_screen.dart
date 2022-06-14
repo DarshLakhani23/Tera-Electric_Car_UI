3ed81178f4620a57292ce19d25af0b8836dd6a66
DarshLakhani23 <darshlakhani23@gmail.com>
2022-06-14 15:18:17 +0530

    #UI improvement

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -33,144 +33,141 @@ class _ProfileState extends State<Profile> {
         title: AppString.profile,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 2.h, horizontal: 6.w),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 4.h,
-                ),
-                SizedBox(
-                  height: 13.h,
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                  color: AppColors.circleavatar, width: 1.5),
-                              boxShadow: const [
-                                BoxShadow(
-                                    offset: Offset(0, 0),
-                                    blurRadius: 4,
-                                    color: AppColors.circleavatar)
-                              ]),
-                          child: const Padding(
-                            padding: EdgeInsets.all(8),
-                            child: CircleAvatar(
-                              radius: 35,
-                              child: Image(
-                                image: AssetImage(ImageAsset.dp),
-                              ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 2.h, horizontal: 6.w),
+          child: Column(
+            children: [
+              Spacer(),
+              SizedBox(
+                height: 13.h,
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                                color: AppColors.circleavatar, width: 1.5),
+                            boxShadow: const [
+                              BoxShadow(
+                                  offset: Offset(0, 0),
+                                  blurRadius: 4,
+                                  color: AppColors.circleavatar)
+                            ]),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: CircleAvatar(
+                            radius: 35,
+                            child: Image(
+                              image: AssetImage(ImageAsset.dp),
                             ),
                           ),
                         ),
                       ),
-                      Positioned(
-                        top: 9.h,
-                        left: 47.w,
-                        child: const CircleAvatar(
-                          radius: 12,
-                          backgroundColor: AppColors.textcolor,
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                            size: 14,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 3.h,
-                ),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      CustomTextField(
-                        readOnly: false,
-                        label: AppString.yourName,
-                        contoller: name,
-                        // validation: (value) {
-                        //   if (value == null || value.isEmpty) {
-                        //     return 'Please enter some text';
-                        //   }
-                        //   return null;
-                        // },
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.next,
-                      ),
-                      CustomTextField(
-                        readOnly: false,
-                        label: AppString.address,
-                        contoller: address,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.next,
-                      ),
-                      CustomTextField(
-                        readOnly: true,
-                        label: AppString.dateOfBirth,
-                        contoller: birthdate,
-                        keyboardType: TextInputType.none,
-                        textInputAction: TextInputAction.next,
-                        onTap: () {
-                          _selectDate(context);
-                        },
-                        suffixIcon: Padding(
-                          padding: const EdgeInsets.all(17.0),
-                          child: SvgPicture.asset(ImageAsset.downArrow),
-                        ),
-                      ),
-                      CustomTextField(
-                        readOnly: false,
-                        // validation: (value) {
-                        //   value!.isEmpty ? "Enter Your Name" : RegExp('[0-9]');
-                        // },
-                        label: AppString.cardInfo,
-                        contoller: cardInfo,
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.done,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 7.h,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // if (formKey.currentState!.validate()) {
-                    // ScaffoldMessenger.of(context).showSnackBar(
-                    //   const SnackBar(content: Text('Processing Data')),
-                    // );
-
-                    // Get.offNamed(Routes.appDrawer);
-                    // arguments: {'name': name.text});
-                    print(name.text);
-                    Get.back();
-                    // }
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          AppColors.textcolor)),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      AppString.editProfile,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 11.sp),
                     ),
+                    Positioned(
+                      top: 9.h,
+                      left: 47.w,
+                      child: const CircleAvatar(
+                        radius: 12,
+                        backgroundColor: AppColors.textcolor,
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                          size: 14,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Spacer(),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      readOnly: false,
+                      label: AppString.yourName,
+                      contoller: name,
+                      // validation: (value) {
+                      //   if (value == null || value.isEmpty) {
+                      //     return 'Please enter some text';
+                      //   }
+                      //   return null;
+                      // },
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    CustomTextField(
+                      readOnly: false,
+                      label: AppString.address,
+                      contoller: address,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    CustomTextField(
+                      readOnly: true,
+                      label: AppString.dateOfBirth,
+                      contoller: birthdate,
+                      keyboardType: TextInputType.none,
+                      textInputAction: TextInputAction.next,
+                      onTap: () {
+                        _selectDate(context);
+                      },
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.all(17.0),
+                        child: SvgPicture.asset(ImageAsset.downArrow),
+                      ),
+                    ),
+                    CustomTextField(
+                      readOnly: false,
+                      // validation: (value) {
+                      //   value!.isEmpty ? "Enter Your Name" : RegExp('[0-9]');
+                      // },
+                      label: AppString.cardInfo,
+                      contoller: cardInfo,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 7.h,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // if (formKey.currentState!.validate()) {
+                  // ScaffoldMessenger.of(context).showSnackBar(
+                  //   const SnackBar(content: Text('Processing Data')),
+                  // );
+
+                  // Get.offNamed(Routes.appDrawer);
+                  // arguments: {'name': name.text});
+                  print(name.text);
+                  Get.back();
+                  // }
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(AppColors.textcolor)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    AppString.editProfile,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 11.sp),
                   ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 2.h,
+              )
+            ],
           ),
         ),
       ),
